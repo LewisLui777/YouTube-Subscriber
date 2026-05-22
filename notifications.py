2f41b7606bda8c310cf91afc1f88f4c183257260
LewisLui777 <121061033+LewisLui777@users.noreply.github.com>
2026-05-22 14:38:32 -0700

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -8,36 +8,29 @@ import sqlite3
 import os
 from googleapiclient.discovery import build
 
-def get_latest_videos(link = 'https://www.youtube.com/@Bosscr/videos'):
-  #Retrieve the HTML from the desired channel
-  url = link
-  headers = {"User-Agent": "Mozilla/5.0"}
-  response = requests.get(url, headers=headers)
-  html = response.text
+def get_latest_videos(playListId,developerKey):
+  # Initialize the YouTube API client
+  youtube = build('youtube', 'v3', developerKey=developerKey)
 
-  #Parse the HTML and find the json text containing information about recent YouTube videos
-  soup = BeautifulSoup(html, "html.parser")
-  script_tag = soup.find("script", string=lambda t: t and "var ytInitialData = " in t)
-  json_text = script_tag.string.strip()[len("var ytInitialData = "):-1]
+  # Request the latest videos from a playlist
+  request = youtube.playlistItems().list(
+      part="snippet",
+      playlistId=playListId,
+      maxResults=5
+  )
+  response = request.execute()
 
-  #Load the json into a dictionary format for easier analysis and extract the relevant metadata
-  data = json.loads(json_text)
-  videos_data = data['contents']['twoColumnBrowseResultsRenderer']['tabs'][1]['tabRenderer']['content']['richGridRenderer']['contents'][:-1]
-  recordings = defaultdict(dict) #title: {length,link,image_url}
-  for i,video in enumerate(videos_data):
-    title = video['richItemRenderer']['content']['videoRenderer']['title']['runs'][0]['text']
-    length = video['richItemRenderer']['content']['videoRenderer']['lengthText']['accessibility']['accessibilityData']['label']
-    link = 'https://www.youtube.com/' + video['richItemRenderer']['content']['videoRenderer']['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
-    image_url = video['richItemRenderer']['content']['videoRenderer']['thumbnail']['thumbnails'][0]['url']
-    recordings[title]['length'] = length
+  recordings = defaultdict(dict)
+  for video in response['items']:
+    title = video['snippet']['title']
+    image_url = video['snippet']['thumbnails']['default']['url']
+    link = 'https://www.youtube.com/watch?v=' + video['snippet']['resourceId']['videoId']
     recordings[title]['link'] = link
     recordings[title]['image_url'] = image_url
-
-
-  #Return the video metadata we found
+    
   return recordings
 
-def save_to_database(database = 'default.db',recordings = defaultdict(dict), name = 'clashroyale'):
+def save_to_database(recordings, name, database = 'default.db'):
   #All tables have columns title, length, link, and image_url
   connection = sqlite3.connect(database)
   cursor = connection.cursor()
@@ -45,18 +38,16 @@ def save_to_database(database = 'default.db',recordings = defaultdict(dict), nam
   #Find the table or create it if it doesn't exist!
   find_table = cursor.execute(f'SELECT name FROM sqlite_master WHERE name="{name}"')
   if find_table.fetchone() == None:
-    cursor.execute(f'CREATE TABLE {name}(title, length, link, image_url)')
+    cursor.execute(f'CREATE TABLE {name}(title, link, image_url)')
 
   new_videos = {}
   for title in recordings.keys():
-    result = cursor.execute(f'SELECT title FROM {name} WHERE title="{title}" AND length="{recordings[title]['length']}"')
+    result = cursor.execute(f'SELECT title FROM {name} WHERE title="{title}" AND link="{recordings[title]['link']}"')
     if result.fetchone() == None:
       #This video doesn't exist in the database yet!
-      new_videos[title] = {'Length': recordings[title]['length'],'Link': recordings[title]['link'],'Image URL': recordings[title]['image_url']}
-      cursor.execute(f'INSERT INTO {name} VALUES ("{title}", "{recordings[title]['length']}", "{recordings[title]['link']}", "{recordings[title]['image_url']}")')
+      new_videos[title] = {'Link': recordings[title]['link'],'Image URL': recordings[title]['image_url']}
+      cursor.execute(f'INSERT INTO {name} VALUES ("{title}", "{recordings[title]['link']}", "{recordings[title]['image_url']}")')
       connection.commit()
-      
-
 
   connection.close()
   return new_videos
@@ -76,13 +67,16 @@ def upload_to_discord(url,new_videos):
     data = {"content": json.dumps(group,indent=4)}
     requests.post(url, json=data)
 
-def notification(webhook,video_link = 'https://www.youtube.com/@Bosscr/videos', database_name = 'clashroyale'):
-  recordings = get_latest_videos(video_link)
+def notification(webhook, playListId, database_name, developerKey):
+  recordings = get_latest_videos(playListId, developerKey)
   new_videos = save_to_database(recordings = recordings, name = database_name)
   upload_to_discord(webhook,new_videos)
 
 if __name__ == "__main__":
   boss_cr_webhook = os.getenv('BOSS_CR_WEBHOOK')
   alt_webhook = os.getenv('ALT_WEBHOOK')
-  notification(boss_cr_webhook)
-  notification(alt_webhook,video_link = 'https://www.youtube.com/@alt3sevens/videos')
+  developer_key = os.getenv('DEVELOPER_KEY')
+  database_name = 'clashroyale'
+  boss_cr_playlist_id = 'UU2PcZfmy7CYrKmFH2-yx_wQ'
+  
+  notification(boss_cr_webhook, boss_cr_playlist_id, database_name, developer_key)
